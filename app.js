db11060bd23a67503a967bfddc7ded9fbda99a38
anshugpt <138896567+anshugpt@users.noreply.github.com>
2023-10-13 14:00:26 +0530

    update app.js
    added function topic

---
+++ app.js
@@ -0,0 +1,37 @@
+//* Function in JS
+// syntax :-
+// function funcName(){
+    // Do soemthing
+// } 
+
+// We need to call a function to use it
+// calling a function :-
+// funcName();
+
+// function hello(){     //* makeing a function
+//     console.log("Hello!"); 
+// }
+
+// hello();  //* using a function
+
+// function loopPrint(a, b){ //* these are called parameters 
+//     for (let i=a; i<=b; i++){
+//         console.log(i);
+//     }
+// }
+
+// loopPrint(10, 20);  //* there are attributes which replace parameters
+
+// function isAdult(){
+//     let age = prompt("What's your age?")
+//     age = parseInt(age)
+//     if (age < 18){
+//         console.warn("You are not adult!!");
+//     } else if (age >= 18){
+//         console.log("You are an Adult!");
+//     } else {
+//         console.error("Invalid input!");
+//     }
+// }
+
+// isAdult();
